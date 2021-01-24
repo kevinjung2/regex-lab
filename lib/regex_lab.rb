f66d04eb74b7a568d3d63aa12b7c2d6c919e13cb
Kevin Jung <kev.jung.ii@gmail.com>
2021-01-23 21:00:50 -0500

    commit tree c087735cd4e76fe0c76679a9f31f7348d41b1d26

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,25 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+  test = word.scan(/^(\b[aeiouAEIOU]\w*\s*)/).length
+  if test == 1
+    return true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/(\bun\w*ing)/).flatten
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-zA-Z]{5}\b/).flatten
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.scan(/^[A-Z].*[.!?]$/) == [] ? false : true
 end
 
 def valid_phone_number?(phone)
-
+  phone.scan(/\(?(\d{3})[\s)-]?\d{3}[\s-]?\d{4}/) == [] ? false : true
 end
